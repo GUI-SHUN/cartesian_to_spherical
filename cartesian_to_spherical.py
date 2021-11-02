d058d675b69581ba9f657d577d6f4d87230e0567
GUI-SHUN <82640571+GUI-SHUN@users.noreply.github.com>
2021-11-02 19:24:09 +0800

    Add files via upload

---
+++ cartesian_to_spherical.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov  2 18:58:59 2021
+
+@author: User
+"""
+import numpy as np
+import math
+
+def cartesian_to_spherical(normal):
+    spherical = np.zeros((normal.shape), dtype=np.float32)
+    for i in range(normal.shape[0]):
+        if normal[i][0]>0 and normal[i][1]>=0:
+            spherical[i][0] = 1.0
+            spherical[i][1] = math.acos(normal[i][2])/math.pi*180
+            spherical[i][2] = abs(math.atan(normal[i][1]/(normal[i][0] + 1e-5))/math.pi*180)
+        elif normal[i][0]<=0 and normal[i][1]>0:
+            spherical[i][0] = 1.0
+            spherical[i][1] = math.acos(normal[i][2])/math.pi*180
+            spherical[i][2] = 180 - abs(math.atan(normal[i][1]/(normal[i][0] - 1e-5))/math.pi*180)
+        elif normal[i][0]<0 and normal[i][1]<=0:
+            spherical[i][0] = 1.0
+            spherical[i][1] = math.acos(normal[i][2])/math.pi*180
+            spherical[i][2] = 180 + abs(math.atan(normal[i][1]/(normal[i][0] - 1e-5))/math.pi*180)
+        elif normal[i][0]>=0 and normal[i][1]<0:
+            spherical[i][0] = 1.0
+            spherical[i][1] = math.acos(normal[i][2])/math.pi*180
+            spherical[i][2] = 360 - abs(math.atan(normal[i][1]/(normal[i][0] + 1e-5))/math.pi*180)
+        else:
+            spherical[i][0] = 1.0
+            spherical[i][1] = 0.0
+            spherical[i][2] = 0.0
+    return spherical
